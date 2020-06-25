1bbf03d755d66967d0718502ca0b49e805660830
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-06-25 09:12:32 +0200

    newly generated project

---
+++ src/main/scala/com/github/aesteve/HttpVerticle.scala
@@ -0,0 +1,24 @@
+package com.github.aesteve
+
+import io.vertx.lang.scala.ScalaVerticle
+import io.vertx.scala.ext.web._
+import io.vertx.scala.core._
+
+import scala.concurrent.Future
+
+class HttpVerticle extends ScalaVerticle {
+
+
+  override def startFuture(): Future[_] = {
+    //Create a router to answer GET-requests to "/hello" with "world"
+    val router = Router.router(vertx)
+    router
+      .get("/hello")
+      .handler(_.response().end("world"))
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listenFuture(8666, "0.0.0.0")
+  }
+}
