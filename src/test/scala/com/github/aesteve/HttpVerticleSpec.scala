84a38c802870d42351845759c7b280cb9853bded
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-06-26 09:35:08 +0200

    reproducer for Julien

--- src/test/scala/com/github/aesteve/HttpVerticleSpec.scala
+++ src/test/scala/com/github/aesteve/HttpVerticleSpec.scala
@@ -1,23 +1,15 @@
 package com.github.aesteve
 
-import io.vertx.scala.ext.web.client._
+import io.vertx.scala.core._
 import org.scalatest.matchers.should.Matchers
 
 class HttpVerticleSpec extends VerticleTesting[HttpVerticle] with Matchers {
 
   "HttpVerticle" should "bind to 8666 and answer with 'world'" in {
-    /*
-    // the expected working version would be:
     vertx.createHttpClient()
-      .getNowFuture(8666, "127.0.0.1", "/hello")
+      .getFuture(8666, "127.0.0.1", "/hello")
       .flatMap(_.bodyFuture)
       .map(_.toString("UTF-8") should equal("world"))
-    // but unfortunately it fails with IllegalStateException (problem with futurised versions of the HttpClient's API)
-     */
-    WebClient.create(vertx, WebClientOptions().setDefaultHost("127.0.0.1").setDefaultPort(8666))
-      .get("/hello")
-      .sendFuture()
-      .map(res => res.bodyAsString should equal("world"))
   }
 
 }
